27d48ac24c946b1cd916906c08333be837809d65
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-07-08 23:56:29 +0100

    deburging

--- gameoflive/src/App.js
+++ gameoflive/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
+import Grid from "./Grid";
+
 
 
 class Box extends React.Component {
@@ -17,31 +19,6 @@ class Box extends React.Component {
   }
 }
 
-class Grid extends React.Component {
-  render() {
-    const width = this.props.cols * 16;
-    var rowsArr = [];
-    var boxClass = "";
-    for (var i = 0; i < this.props.rows; i++) {
-      for (var j = 0; j < this.props.rows; j++) {
-        let boxid = i + "_" + j;
-        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
-        rowsArr.push(
-          <Box
-            boxClass={boxClass}
-            key={boxid}
-            boxid={boxid}
-            row={i}
-            col={j}
-            selectBox={this.props.selectBox} />
-        );
-      }
-    }
-    return (
-      <div className="grid" style={{ width: width }} >{rowsArr} </div>
-    )
-  }
-}
 
 class App extends React.Component {
   constructor() {
--- gameoflive/src/Grid.js
+++ gameoflive/src/Grid.js
@@ -1,1 +1,31 @@
 import React from "react";
+
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 16;
+    var rowsArr = [];
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.rows; j++) {
+        let boxid = i + "_" + j;
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxid}
+            boxid={boxid}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      <div className='grid' style={{ width: width }}>
+        {rowsArr}{" "}
+      </div>
+    );
+  }
+}
+export default Grid;
