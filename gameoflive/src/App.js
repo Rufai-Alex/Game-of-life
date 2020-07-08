6c0ac036580103eb09014ccb5dff0fc4d0d99e36
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-07-08 13:24:46 +0100

    Create-Box-Component

--- gameoflive/src/App.js
+++ gameoflive/src/App.js
@@ -2,9 +2,24 @@ import React, { useState } from "react";
 import "./App.css";
 
 
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.rows, this.props.col);
+  }
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
+      />
+    );
+  }
+}
+
 class Grid extends React.Component {
   render() {
-    const width = this.props.cols * 14;
+    const width = this.props.cols * 16;
     var rowsArr = [];
     var boxClass = "";
     for (var i = 0; i < this.props.rows; i++) {
@@ -19,11 +34,11 @@ class Grid extends React.Component {
             row={i}
             col={j}
             selectBox={this.props.selectBox} />
-        )
+        );
       }
     }
     return (
-      <div className="grid" style={{ width: width }} >{{ rowsArr }} </div>
+      <div className="grid" style={{ width: width }} >{rowsArr} </div>
     )
   }
 }
@@ -31,19 +46,19 @@ class Grid extends React.Component {
 class App extends React.Component {
   constructor() {
     super();
-    this.speed = 100,
-      this.cols = 40,
-      this.rows = 50,
-      this.state = {
-        generation: 0,
-        gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
-      }
+    this.speed = 100;
+    this.cols = 50;
+    this.rows = 50;
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    }
   }
   render() {
     return (
       <div className='App' >
         <h1>Game Of Life</h1>
-        <Grid gridFull={this.state.gridFull} rows={this.rows} cols={this.cols} selectBox={selectBox} />
+        <Grid gridFull={this.state.gridFull} rows={this.rows} cols={this.cols} selectBox={this.selectBox} />
         <h2>generation: {this.state.generation} </h2>
 
       </div>
